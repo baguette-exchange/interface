34d7dfe717b144c99b7549d76a1f7c77a2a1a950
My Baker <dev@baguette.exchange>
2022-01-31 21:18:56 +0100

    Update info for batch #10

--- src/pages/Mill/index.tsx
+++ src/pages/Mill/index.tsx
@@ -104,7 +104,7 @@ export default function Mill() {
         <DataRow style={{ alignItems: 'baseline' }}>
           <TYPE.mediumHeader style={{ marginTop: '0.5rem' }}>Participating pools</TYPE.mediumHeader>
           <TYPE.black fontWeight={400}>
-            Baguette batch #9 ends on January 31st
+            Baguette batch #10 ends on February 28th
           </TYPE.black>
         </DataRow>
         <AutoRow justify="flex-end">
