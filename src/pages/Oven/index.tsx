2b0c56577cc1b1d26354fa4940aad880068680b5
My Baker <dev@baguette.exchange>
2022-08-06 19:29:34 +0200

    Update info for batch #16

--- src/pages/Oven/index.tsx
+++ src/pages/Oven/index.tsx
@@ -105,7 +105,7 @@ export default function Oven() {
         <DataRow style={{ alignItems: 'baseline' }}>
           <TYPE.mediumHeader style={{ marginTop: '0.5rem' }}>Participating pools</TYPE.mediumHeader>
           <TYPE.black fontWeight={400}>
-            Baguette batch #15 ends on July 31st
+            Baguette batch #16 ends on September 1st
           </TYPE.black>
         </DataRow>
         <AutoRow justify="flex-end">
